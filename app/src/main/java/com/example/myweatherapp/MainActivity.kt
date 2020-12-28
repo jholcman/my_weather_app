7226448706273fe7f5c72cc9f7dba27cddb19a5f
jholcman <nikita.moda@volny.cz>
2020-12-28 16:05:04 +0100

    Splash Screen

--- app/src/main/java/com/example/myweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/myweatherapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.myweatherapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+    // Splash screen timer
+    private val SPLASH_TIME_OUT = 3000L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler().postDelayed(
+            {
+                val i = Intent(this@MainActivity, HomeActivity::class.java)
+                startActivity(i)
+                finish()
+            }, SPLASH_TIME_OUT)
     }
 }
